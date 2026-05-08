a3f6d0c93a801a1916a7945594af790a948af646
Mehedi Hasan <u1904067@student.cuet.ac.bd>
2026-05-08 18:59:49 +0600

    initialize the project

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+
+
+const app: Application = express();
+const port = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.json());
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript + Express!');
+});
+
+export default app;
