966d2197e6336f94eef41096ae01786881a7b4ef
Mehedi Hasan <u1904067@student.cuet.ac.bd>
2026-05-17 15:31:29 +0600

    fix: revert PrismaClient import to use locally generated client

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client/extension";
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "../../generated/prisma";
 
 const connectionString = "postgresql://neondb_owner:npg_IWB6aPXj5QsS@ep-polished-sunset-a8o5ndqa-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require";
 
